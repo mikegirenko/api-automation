2954fff1b96d0b676bb77bce6ce7c695d29e9afe
Mike Girenko <mike.girenko@processunity.com>
2025-08-14 13:04:06 -0600

    Added pytest.ini

--- spoonacular/tests/test_search_recipes.py
+++ spoonacular/tests/test_search_recipes.py
@@ -1,13 +1,21 @@
 import pytest
 import requests
+import logging
 
 from spoonacular.common.common import API_KEY, BASE_URL
 from spoonacular.request.headers import HEADERS
 
+logger = logging.getLogger(__name__)
 
-# Search through thousands of recipes using advanced filtering and ranking
-
+# Search through recipes
+@pytest.mark.all_tests
 def test_search():
+    logger.info("Starting search test")
     URL = BASE_URL + "recipes/complexSearch?query=" + "Apple" + API_KEY
     response = requests.request("GET", URL, headers=HEADERS)
     print(response.json())
+
+    logger.info("Ending search test")
+
+#  pytest -m all_tests
+#  pytest -s test_search_recipes.py::test_search
